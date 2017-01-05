1540091b464c8763d43348ff8310cff3a4039ceb
Noah Berman <noah@noahberman.org>
2017-01-05 11:53:20 -0700

    config for feature and unit tests

--- test/specs/views.js
+++ test/specs/views.js
@@ -2,19 +2,6 @@ var assert = require('assert');
 var Launcher = require('webdriverio').Launcher;
 var webdriverio = require('wdio-mocha-framework');
 var wdio = new Launcher('./wdio.conf.js');
-wdio.run().then(function (code) {
-    process.exit(code);
-}, function (error) {
-    console.error('Launcher failed to start the test', error.stacktrace);
-    process.exit(1);
-});
-var options = {
-    desiredCapabilities: {
-        browserName: 'firefox'
-    }
-};
-
-
 
 describe('main view page', function() {
   it('should have the right title', function() {
