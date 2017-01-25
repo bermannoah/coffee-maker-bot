b0a957c270da245a7f7f12950e181041cb30a248
Noah Berman <noah@noahberman.org>
2017-01-24 21:50:32 -0700

    fix missing bracket

--- app.js
+++ app.js
@@ -13,25 +13,7 @@ var thingShadows = awsIot.thingShadow({
 });
 
 thingShadows.on('connect', function() {
-//
-// After connecting to the AWS IoT platform, register interest in the
-// Thing Shadow named 'RGBLedLamp'.
-//
     thingShadows.register( 'coffee-maker-bot', function() {
-
-// Once registration is complete, update the Thing Shadow named
-// 'RGBLedLamp' with the latest device state and save the clientToken
-// so that we can correlate it with status or timeout events.
-//
-// Thing shadow state
-////
-// The update method returns a clientToken; if non-null, this value will
-// be sent in a 'status' event when the operation completes, allowing you
-// to know whether or not the update was successful.  If the update method
-// returns null, it's because another operation is currently in progress and
-// you'll need to wait until it completes (or times out) before updating the 
-// shadow.
-//
        if (clientTokenUpdate === null)
        {
           console.log('update shadow failed, operation still in progress');
@@ -42,13 +24,6 @@ thingShadows.on('status',
     function(thingName, stat, clientToken, stateObject) {
        console.log('received '+stat+' on '+thingName+': '+
                    JSON.stringify(stateObject));
-//
-// These events report the status of update(), get(), and delete() 
-// calls.  The clientToken value associated with the event will have
-// the same value which was returned in an earlier call to get(),
-// update(), or delete().  Use status events to keep track of the
-// status of shadow operations.
-//
     });
 
 thingShadows.on('delta', 
@@ -61,13 +36,8 @@ thingShadows.on('timeout',
     function(thingName, clientToken) {
        console.log('received timeout on '+thingName+
                    ' with token: '+ clientToken);
-//
-// In the event that a shadow operation times out, you'll receive
-// one of these events.  The clientToken value associated with the
-// event will have the same value which was returned in an earlier
-// call to get(), update(), or delete().
-//
     });
+});
 
 mongoose.connect(config.db);
 var db = mongoose.connection;
