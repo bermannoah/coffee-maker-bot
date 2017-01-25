c4c53dc3826e9a01855debf0e18ada48500a8592
Noah Berman <noah@noahberman.org>
2017-01-24 22:16:58 -0700

    remove aws extras

--- app.js
+++ app.js
@@ -2,42 +2,6 @@ var express = require('express');
 var config = require('./config/config');
 var glob = require('glob');
 var mongoose = require('mongoose');
-var awsIot = require('aws-iot-device-sdk');
-
-var thingShadows = awsIot.thingShadow({
-   keyPath: "../aws-iot/coffee-maker-bot.private.key",
-  certPath: "../home/noah/aws-iot/coffee-maker-bot.cert.pem",
-    caPath: "../home/noah/aws-iot/root-CA.crt",
-  clientId: "coffee-maker-bot",
-    region: "us-west-2"
-});
-
-thingShadows.on('connect', function() {
-    thingShadows.register( 'coffee-maker-bot', function() {
-       if (clientTokenUpdate === null)
-       {
-          console.log('update shadow failed, operation still in progress');
-       }
-    });
-
-thingShadows.on('status', 
-    function(thingName, stat, clientToken, stateObject) {
-       console.log('received '+stat+' on '+thingName+': '+
-                   JSON.stringify(stateObject));
-    });
-
-thingShadows.on('delta', 
-    function(thingName, stateObject) {
-       console.log('received delta on '+thingName+': '+
-                   JSON.stringify(stateObject));
-    });
-
-thingShadows.on('timeout',
-    function(thingName, clientToken) {
-       console.log('received timeout on '+thingName+
-                   ' with token: '+ clientToken);
-    });
-});
 
 mongoose.connect(config.db);
 var db = mongoose.connection;
